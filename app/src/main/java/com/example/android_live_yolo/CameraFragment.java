690c6d9e5a06fdedc2b7761a6a1d3042511409ea
chaeheejo <60cogml@gmail.com>
2022-07-25 12:06:41 +0900

    success to bind camera preview

--- app/src/main/java/com/example/android_live_yolo/CameraFragment.java
+++ app/src/main/java/com/example/android_live_yolo/CameraFragment.java
@@ -1,28 +1,18 @@
 package com.example.android_live_yolo;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
+import android.Manifest;
 import android.os.Bundle;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LifecycleOwner;
 
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,6 +20,7 @@ import android.view.ViewGroup;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.io.File;
 import java.util.concurrent.ExecutionException;
 
 public class CameraFragment extends Fragment {
@@ -61,36 +52,19 @@ public class CameraFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @NonNull Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        previewView = view.findViewById(R.id.camera_preview);
 
+        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, 1);
+        cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext());
 
-        previewView = view.findViewById(R.id.camera_view);
-
-        setCameraProviderFuture();
-        Log.d("home", "finish camera provider");
-    }
-
-    private void setCameraProviderFuture(){
-        //cameraProvider 요청
-        try{
-            cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext());
-        }catch (Exception e){
-            Log.e("home", "setCameraProviderFuture: ", e);
-        }
-
-
-        //뷰를 만들 때 preview를 제공
         cameraProviderFuture.addListener(() -> {
             try {
                 ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
                 bindPreview(cameraProvider);
             } catch (ExecutionException | InterruptedException e) {
-                // No errors need to be handled for this Future.
-                // This should never be reached.
+                Log.e("home", "at listener: ", e);
             }
         }, ContextCompat.getMainExecutor(requireContext()));
-
-
-        Log.d("home", "-ing camera provider");
     }
 
     private void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
@@ -103,8 +77,6 @@ public class CameraFragment extends Fragment {
 
         preview.setSurfaceProvider(previewView.getSurfaceProvider());
 
-        Camera camera = cameraProvider.bindToLifecycle(getViewLifecycleOwner(), cameraSelector, preview);
-
-        Log.d("home", "finish bindPreviews");
+        cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
     }
 }
